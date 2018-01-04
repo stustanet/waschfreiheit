5c9d710fd11b790a5ce6439768ee546f32c4d3cf
Daniel Frejek <daniel.frejek@stusta.net>
2018-01-04 20:43:04 +0100

    Use own board "wasch_v1" and fixed build for current RIOT version

--- mcu_riot/node/header/meshnw_config.h
+++ mcu_riot/node/header/meshnw_config.h
@@ -1,31 +1,10 @@
 #pragma once
 
-// RF Transceiver config
-// The SPI interface the SX127X is connected to.
-#define SX127X_PARAM_SPI                    (SPI_DEV(0))
-
-// Speed of the SPI
-#define SX127X_PARAM_SPI_SPEED              (SPI_CLK_1MHZ)
-
-// SPI mode
-#define SX127X_PARAM_SPI_MODE               (SPI_MODE_0)
-
-// NSS is connected to this GPIO
-#define SX127X_PARAM_SPI_NSS                GPIO_PIN(0, 4)       /* A4 */
-
-// RESET is connected to this GPIO
-#define SX127X_PARAM_RESET                  GPIO_PIN(2, 15)      /* C15 */
-
-// Digital IO pins of the SX127X are connected to the following GPIOs
-#define SX127X_PARAM_DIO0                   GPIO_PIN(0, 3)       /* A3 */
-#define SX127X_PARAM_DIO1                   GPIO_PIN(0, 2)       /* A2 */
-#define SX127X_PARAM_DIO2                   GPIO_PIN(0, 1)       /* A1 */
-#define SX127X_PARAM_DIO3                   GPIO_PIN(0, 0)       /* A0 */
-
+#include "net/lora.h"
 
 // RF configuration
 // See the SX127X datasheet for details.
-#define SX127X_CONFIG_LORA_BW        SX127X_BW_125_KHZ
+#define SX127X_CONFIG_LORA_BW        LORA_BW_125_KHZ
 #define SX127X_CONFIG_LORA_SPREAD    12
 #define SX127X_CONFIG_LORA_CODERATE  6
 
