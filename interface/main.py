a92270968d145eb8a296f9a2446e8c67ef1a8c6e
johannes walcher <johannes.walcher@stusta.de>
2018-02-08 21:20:15 +0100

    Rewrite of most of the async stuff, quite stable

--- interface/main.py
+++ interface/main.py
@@ -1,109 +1,213 @@
 #!/usr/bin/env python3
+import asyncio
+import argparse
+import time
 import wasch
 import machinemanager
-import asyncio
-
-async def observer(master):
-    while True:
-        await asyncio.sleep(1)
-        if not master:
-            continue
-        state =  "*************************************************\n"
-        state += "* Master:\n"
-        state += "*  - response_pending: {}\n".format(master.response_pending)
-        state += "*  - message_pending:  {}\n".format(master.message_pending)
-        state += "*  - last_command:     {}\n".format(master.last_command)
-        state += "*     - messagestr:    {}\n".format(master.last_command.message)
-        state += "*     - retransmit:    {}\n".format(master.last_command.retransmit)
-        state += "*     - r_count:       {}\n".format(master.last_command.retransmit_count)
-        state += "*\n"
-        state += "*  - Sensors [{}]:\n".format(len(master._sensors))
-        for id, s in master._sensors.items():
-            state += "*\n"
-            state += "*    # SENSOR: ID {}: {}\n".format(s.nodeid, s.config.name)
-            state += "*      - last_distance: {}\n".format(s.distance)
-            state += "*      - last_status:   {}\n".format(s.last_status)
-            state += "*      - last_update:   {}\n".format(s.last_update)
-            state += "*      - state:         {}\n".format(s.state)
-            if master.last_command.node == s:
-                state += "*      - running command: {}\n".format(master.last_command)
-
-        state +=  "\n\n"
-
-        with open("/tmp/wasch.state", 'w+') as f:
-            f.write(state)
 
 
-async def network_sanity_observer(master):
-    print("Starting network sanity observer every {} seconds".format(
-        master.config.networkcheckintervall))
+class WaschenInFreiheit:
+    """
+    Main management interface that runs all observer jobs and tries to make
+    sense of all data available. Also here for glueing the website to status
+    changes
+    """
 
-    while True:
-        await asyncio.sleep(master.config.networkcheckintervall)
+    def __init__(self, configfile, serial_port, loop=None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        self.loop = loop
+
+        self.master = wasch.WaschInterface(
+            serial_port,
+            configfile,
+            loop=self.loop)
+        self.observertask = loop.create_task(self.observer())
+        self.networksanitizertask = None
+        self.machineobserver = None
+        self.last_observer_run = None
+        self.next_observer_run = None
+        self.startup_time = 0
+        self.startup = time.time()
+
+    async def teardown(self):
+        """
+        Retrieve the results of all async tasks
+        """
         try:
-            print("Performing network sanity check")
-            await master.networkmanager.recover_network(None)
-        except wasch.WaschOperationInterrupted:
-            print("Network sanity check was interrupted")
+            if self.machineobserver:
+                await self.machineobserver.teardown()
+        except asyncio.CancelledError:
+            pass
+        try:
+            await self.master.teardown()
+        except asyncio.CancelledError:
             pass
 
-def node_status_update(node, status):
-    print("\n\nNODE {} HAS NEW STATUS: {}\n\n".format(node, status))
+        try:
+            if self.observertask:
+                self.observertask.cancel()
+                await self.observertask
+        except asyncio.CancelledError:
+            pass
 
-async def run(master):
-    # TODO Here the magic happens:
-    #
-    # Magic auth-ping to keep testing the network to all nodes
-    # reboot identification
-    # Error handling for failed nodes:
-
-    # Configure the master
-    master.status_subscribe(node_status_update)
-    mm = machinemanager.MachineManager(master, "http://waschen.stusta.de", "token")
-
-    try:
-        await master.get_node("HSH16").authping()
-    except wasch.WaschError:
-        print("Wasch has failed")
+        try:
+            if self.networksanitizertask:
+                self.networksanitizertask.cancel()
+                await self.networksanitizertask
+        except asyncio.CancelledError:
+            pass
 
 
-if __name__ == "__main__":
+    async def start(self):
+        """
+        Start the execution, afterwards the event loop should be run forever
+        """
+        await self.master.start()
+        self.startup_time = int((time.time() - self.startup) * 1000) / 1000
+        try:
+            await self.master.get_node("HSH16").authping()
+            self.networksanitizertask = self.loop.create_task(
+                self.network_sanity_observer())
+
+            self.machineobserver = machinemanager.MachineManager(
+                self.master, "http://waschen.stusta.de",
+                "token", loop=self.loop)
+        except wasch.WaschError:
+            self.master.log.error("Wasch startup has failed")
+
+    async def observer(self):
+        """
+        Keep track of the internal state of the waschinterface. This is a much
+        less verbose interface than the stdout, where everything is printed.
+        This provides an overview over what is being done
+        """
+        while True:
+            await asyncio.sleep(1)
+            if not self.master:
+                continue
+            state = "*************************************************\n"
+            state += "* Update time: {}\n".format(time.asctime())
+            if self.next_observer_run:
+                state += "* Next network observer run in {}s\n"\
+                         .format(int(self.next_observer_run - time.time()))
+            else:
+                state += "* Next network observer run: None\n"
+
+            if self.last_observer_run:
+                state += "* Last observer run {}s ago\n"\
+                         .format(int(time.time() - self.last_observer_run))
+            else:
+                state += "* Last observer run: None\n"
+
+            if self.startup_time:
+                state += "* Startup time was: {}s\n"\
+                         .format(self.startup_time)
+            else:
+                state += "* Startup time was: Still waiting\n"
+
+            if self.machineobserver:
+                state += "* Full network startup: {}s\n"\
+                         .format(int(self.machineobserver.all_there
+                                     - self.startup))
+            else:
+                state += "* Full network startup: Still waiting...\n"
+
+            state += "* Master:\n"
+            state += "*  - message_pending:  {}\n"\
+                     .format(self.master.message_pending)
+            if self.master.running_message:
+                msg = self.master.running_message
+                state += "*  - last_command:     {}\n".format(msg)
+                state += "*     - messagestr:    {}\n".format(msg.message)
+                state += "*     - retransmit:    {}\n".format(msg.retransmit)
+                state += "*     - r_count:       {}\n"\
+                         .format(msg.retransmit_count)
+            state += "*\n"
+            state += "*  - Sensors [{}]:\n".format(len(self.master.sensors))
+            for _, snsor in self.master.sensors.items():
+                state += "*\n"
+                state += "*    # SENSOR: {} ({})\n"\
+                         .format(snsor.config.name, snsor.nodeid)
+                state += "*      - distance:      {}\n".format(snsor.distance)
+                state += "*      - last_status:   {}\n"\
+                         .format(snsor.last_status)
+                state += "*      - last_update:   {}\n"\
+                         .format(snsor.last_update)
+                state += "*      - state:         {}\n".format(snsor.state)
+                if self.master.running_message.node == snsor:
+                    state += "*      - running command: {}\n"\
+                             .format(self.master.running_message)
+
+            state += "\n\n"
+
+            with open("/tmp/wasch.state", 'w+') as statefile:
+                statefile.write(state)
+
+
+    async def network_sanity_observer(self):
+        """
+        In regular intervals start a network recovery task.
+        The network recovery starts with pinging all nodes an assessing, what
+        part of the network does neet do be recovered, so it gets in contact
+        with every node
+
+        This should not be done too often, since it requires a lot of bandwidth
+        """
+        self.master.log.info("Start network sanity observer every %s seconds",
+                             self.master.config.networkcheckintervall)
+
+        while True:
+            timeout = self.master.config.networkcheckintervall
+
+            self.next_observer_run = time.time() + timeout
+            await asyncio.sleep(timeout)
+            self.last_observer_run = time.time()
+            try:
+                self.master.log.info("Performing network sanity check")
+                await self.master.networkmanager.recover_network(None)
+            except wasch.WaschOperationInterrupted:
+                pass
+            except wasch.WaschCommandError as exp:
+                self.master.log.exception(exp)
+
+
+def main():
     loop = asyncio.get_event_loop()
-
+    loop.set_debug(True)
     # In order to test the interface open a virtual socket using
     # `socat -d -d pty,raw,echo=0 pty,raw,echo=0`
     # And enter its /dev/pts/xx file here
 
     # if you have a real serial interface, just issue the serial /dev/ttyUSBX
     # here.
+    parser = argparse.ArgumentParser(description="Start the wasch-master")
+    parser.add_argument("-c", "--configfile", help="configfile to use")
+    parser.add_argument("-s", "--serial", help="Serial port to use")
+    args = parser.parse_args()
 
+    if args.configfile:
+        configfile = args.configfile
+    else:
+        configfile = "nodes.json"
+    if args.serial:
+        serial_port = args.serial
+    else:
+        serial_port = "/tmp/waschfreiheit_pts"
 
-    master = wasch.WaschInterface(
-        #"/tmp/waschfreiheit_pts",
-        "/dev/ttyUSB0",
-        'nodes.json', loop=loop)
-
-
-    observertask = loop.create_task(observer(master))
-
+    waschen = WaschenInFreiheit(configfile, serial_port, loop=loop)
     try:
-        loop.run_until_complete(master.start())
-        loop.run_until_complete(run(master))
-        networksanitizertask = loop.create_task(network_sanity_observer(master))
+        loop.run_until_complete(waschen.start())
+        waschen.master.log.info("Done with startup")
         loop.run_forever()
-    except:
-        raise
-        pass
+    except KeyboardInterrupt:
+        waschen.master.log.error("Cancelled by Keyboard.")
     finally:
-        try:
-            observertask.cancel()
-            observertask.result()
-        except (asyncio.CancelledError, asyncio.InvalidStateError):
-            pass
-        try:
-            networksanitizertask.cancel()
-            networksanitizertask.result()
-        except asyncio.CancelledError:
-            pass
+        waschen.master.log.info("Starting teardown")
+        loop.run_until_complete(waschen.teardown())
+        waschen.master.log.info("Teardown complete."
+                                "Goodbye and thanks for all the fish")
+        loop.stop()
 
-    loop.cancel()
+if __name__ == "__main__":
+    main()
