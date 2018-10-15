38aef80035712b435524f27e1bf5204c83639992
Daniel Frejek <daniel.frejek@stusta.net>
2018-10-15 23:16:08 +0200

    Added FreeRTOS config and minimal main

--- firmware/source/main.c
+++ firmware/source/main.c
@@ -9,24 +9,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <thread.h>
-#include <shell.h>
-#include <shell_commands.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/flash.h>
+#include <libopencm3/cm3/cortex.h>
 
-#include <board.h>
-#include <periph/rtc.h>
+#include <FreeRTOS.h>
+#include <task.h>
 
-#include "utils.h"
-#include "serial_getchar_dma.h"
+// Clock for the STM32F401
+const struct rcc_clock_scale hse_8_84mhz =
+{
+	.pllm = 4,     // PLL input is 2MHz
+	.plln = 168,   // PLL output is 336 MHz
+	.pllp = 4,     // CPU clock 84 MHz
+	.pllq = 7,     // USB clock 48MHz
+	.hpre = RCC_CFGR_HPRE_DIV_NONE,
+	.ppre1 = RCC_CFGR_PPRE_DIV_2,
+	.ppre2 = RCC_CFGR_PPRE_DIV_NONE,
+	.flash_config = FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_LATENCY_2WS,
+	.ahb_frequency  = 84000000,
+	.apb1_frequency = 42000000,
+	.apb2_frequency = 84000000,
+};
+
+#define CLOCK_SETTINGS hse_8_84mhz
 
 
 int main(void)
 {
-	serial_getchar_dma_init();
-	init();
+	cm_disable_interrupts();
+	rcc_clock_setup_hse_3v3(&CLOCK_SETTINGS);
 
-	char line_buf[256];
-	shell_run(shell_commands, line_buf, sizeof(line_buf));
+	//init();
+
+	vTaskStartScheduler();
 
 	return 0;
+}
+
+
+// From https://www.freertos.org/a00110.html
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
+                                    StackType_t **ppxIdleTaskStackBuffer,
+                                    uint32_t *pulIdleTaskStackSize )
+{
+	static StaticTask_t xIdleTaskTCB;
+	static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
