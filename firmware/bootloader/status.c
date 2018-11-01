295866d1711fa5607b925d9e98bfdbe994c39e5d
Daniel Frejek <daniel.frejek@stusta.net>
2018-11-01 19:28:44 +0100

    This should be MIT, not GPL

--- firmware/bootloader/status.c
+++ firmware/bootloader/status.c
@@ -1,17 +1,7 @@
 /*
- * Copyright (c) 2018 Daniel Frejek.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2018 Daniel Frejek
+ * This source code is licensed under the MIT license that can be found
+ * in the LICENSE file.
  */
 
 #include "status.h"
