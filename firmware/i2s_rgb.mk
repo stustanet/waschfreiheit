d8b18a255fcfe5134748a58b5f878944f8155ff0
Daniel Frejek <daniel.frejek@stusta.net>
2018-11-01 14:48:49 +0100

    Moved i2s led impl to own dir

---
+++ firmware/i2s_rgb.mk
@@ -0,0 +1,5 @@
+vpath %.c i2s_rgb
+
+TGT_CFLAGS += -Ii2s_rgb
+
+OBJS += i2s_rgb.o
--- firmware/source/sensor/sensor.mk
+++ firmware/source/sensor/sensor.mk
@@ -4,7 +4,6 @@ ifeq ($(VERSION),V1)
 FILES += led_ws2801
 FILES += stm32f1_flasher
 else
-FILES += i2s_rgb
 FILES += usb_storage_helper debug_command_queue debug_file_logger storage_manager frequency_sensor
 endif
 
