5fb283a9eae2bc6676a81ef356b2a559abb8ee93
Daniel Frejek <daniel.frejek@stusta.net>
2018-11-02 20:36:09 +0100

    Added sx127x test command to the master node

--- firmware/source/master/master_main.c
+++ firmware/source/master/master_main.c
@@ -49,6 +49,7 @@ const cli_command_t cli_commands[] = {
     { "rebuild_status_channel", "Request a node to rebuild the status channel", master_node_cmd_rebuild_status_channel },
     { "cfg_status_change_indicator", "Configure the status change indicator LEDs.", master_node_cmd_configure_status_change_indicator },
     { "routes",        "Sets the routes for the master node",  cmd_routes },
+    { "sx127x",           "RF modem debug",                    sx127x_test_cmd },
     { "reboot",        "Reboots (resets) the MCU",  cmd_reboot },
     { NULL, NULL, NULL }
 };
